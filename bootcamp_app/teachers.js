c867168b322ebd1ff7e67438f84f31cf2eca4e1d
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-12-14 01:46:33 -0600

    prevented sql injection attack

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,19 @@ const pool = new Pool({
 });
 
 
+const teacherName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [`%${teacherName}%`];
+
 pool.query(`
 SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`,values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort} : ${user.teacher}`);
